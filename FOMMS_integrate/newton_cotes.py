7d61263976d00b91aeafa5f0ee5724633929cdac
Trina M Jensen <trje3733@colorado.edu>
2018-07-31 10:43:44 -0600

    test 1

--- FOMMS_integrate/newton_cotes.py
+++ FOMMS_integrate/newton_cotes.py
@@ -6,6 +6,34 @@ Handles the primary functions
 """
 
 
+"""
+This file contains the implementation of the trapezoidal rule
+"""
+
+import numpy as np
+
+def trapz(x, f):
+    """ 
+    Compute a 1D definite integral using the trapezoidal rule
+    Parameters
+    ----------
+    f : function
+        User defined function.
+    x : numpy array
+        Integration domain.
+    Returns
+    -------
+    I : float
+        Integration result.
+    """   
+    a = x[0]
+    b = x[1]
+    ya = f(a)
+    yb = f(b)
+    I = (b-a) * (ya + yb) / 2
+    return I
+
+
 def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
--- FOMMS_integrate/tests/test_FOMMS_integrate.py
+++ FOMMS_integrate/tests/test_FOMMS_integrate.py
@@ -3,10 +3,35 @@ Unit and regression test for the FOMMS_integrate package.
 """
 
 # Import package, test suite, and other packages as needed
-import FOMMS_integrate
+import FOMMS_integrate as integrate
 import pytest
 import sys
 
 def test_FOMMS_integrate_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "FOMMS_integrate" in sys.modules
+
+
+"""
+Testing the integrate package
+"""
+
+import numpy as np
+
+def g(x):
+    return 3.0 * x
+
+def f(x):
+    return np.power(x, 2)
+
+def h(x):
+    return np.ones(x.size)
+
+def volume(x):
+    squares = np.power(x, 2)
+    return np.sum(squares, axis=1)
+
+def test_trapz():
+    x = np.array([0, 10])
+    I = integrate.trapz(x, g)
+    assert I == 150.00
